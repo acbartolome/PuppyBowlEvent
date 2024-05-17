6b969aa14a7e16ab73bbb0a220563ce2c409252e
adryanbartolome <adryanbartolome8123@gmail.com>
2024-05-16 21:08:38 -0700

    Set up the files and copied starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import the React library, which allows us to define components
 
+
+// Import the Players component, which we'll use to show a list of players
+
+
+// Define the App component
 function App() {
-  const [count, setCount] = useState(0)
-
+  // This component renders the Players component inside a div
+  // This div has a class of 'App', which we could use for styling
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+  );
 }
 
-export default App
+// Export the App component as the default export
