a3820991d4bc1e098bdfa48ef07b0fa136f5eef6
adryanbartolome <adryanbartolome8123@gmail.com>
2024-05-18 10:02:40 -0700

    Completed the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,27 @@ import React from "react";
 import ReactDOM from 'react-dom/client'
 
 // Import the main App component
-import
+import App from './App'
 
 // Import the CSS file
-
+import './App.css'
+import './index.css'
 
 // Import the Redux store
 // The store is where you keep the app's state
-
+import { store } from "./app/store";
 
 // Import the Provider component from react-redux
 // Provider makes the Redux store available to any nested components that need to access it
-
+import { Provider } from "react-redux";
 
 // Call ReactDOM.render to render our App component in the DOM
 // Wrap the App in a Provider to give it access to the Redux store
 // Also, wrap it in React.StrictMode, which is a wrapper component that checks for potential problems in the app during development
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
+  <Provider store={store}>
+    <App />
+  </Provider>
   </React.StrictMode>,
-
 );
